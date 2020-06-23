e18ae2b78e5b20bc77b899e49692bf45b77192c9
Anna James-Bott <anna.james-bott@st-hildas.ox.ac.uk>
2020-06-23 11:42:11 +0100

    Some changes had reverted back to old file, changed back so pipeline handles creating dir, defaults removed from optparse etc.

--- R/GTF_maker.R
+++ R/GTF_maker.R
@@ -21,19 +21,20 @@
 library(optparse)
 parser <- OptionParser()
 option_list <- list( 
-  make_option(c("-o", "--outputdir"), action="store", default='/well/immune-rep/users/kvi236/VIRUS/TEST', type="character", help="Path to output directory"),
-  make_option(c("-i", "--indexgenome"), action="store", type="character", default="/gpfs2/well/immune-rep/users/kvi236/VIRAL_TRACK_REFERENCE_BUILD_273a", help="Path to VIRAL TRACK reference genome [default]")
+  make_option(c("-o", "--outfile"), action="store", type="character", help="Path to output GTF file"),
+  make_option(c("-i", "--indexgenome"), action="store", type="character", help="Path to VIRAL TRACK reference genome")
 )
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser, print_help_and_exit = TRUE, args = commandArgs(trailingOnly = TRUE) )
+
+outfile <- opt$outfile
+
 ## Reading in chrLength file from the STAR genome Index build: 
 backbone <- paste0(opt$indexgenome, '/chrNameLength.txt')
 ChrLength <- read.delim(backbone, header=FALSE)
-## Making GTF directory in Outs 
-GTF_dir = paste(opt$outputdir, '/GTF', sep = "")
-dir.create(GTF_dir)
-Path_GTF = paste(GTF_dir, "/ViralTrack_GTF.gtf", sep="")
-fileConn <- file(Path_GTF, open = "w+")
+
+fileConn <- file(outfile, open = "w+")
+
 #Writing in GTF format  
 for(i in 1:length(ChrLength[,1])){
   z = paste(ChrLength$V1[i],'RefSeq', 'transcript', 1, ChrLength$V2[i],1000,".",".", paste("gene_id ", ChrLength$V1[i] ,"_1;", sep =""), sep='\t')
